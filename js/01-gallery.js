78b0fe957645b124001ae0cfcdd8facacb152bd8
Andrii-Vekker <vekker304@gmail.com>
2022-07-11 23:32:31 +0300

    dwqdw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// const instance = basicLightbox.create(`
+// <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>
+// `);
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.addEventListener("click", galleryHandler)
+
+function createMurkup(items) {
+    const markup = galleryItems.map(i => 
+`<div class="gallery__item">
+  <a class="gallery__link" href="${i.original}">
+    <img
+      class="gallery__image"
+      src="${i.preview}"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>`)
+return galleryRef.createElement(markup)
+};
+createMurkup(galleryItems)
+
+
+
+function galleryHandler(e) {
+  
+}
+
 console.log(galleryItems);
+
+
